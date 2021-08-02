b91cd7235c8a4b19cf47fc909e90b653f8a6ae4d
Zac <a19860101@gmail.com>
2021-08-02 21:03:25 +0800

    20210802

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -8,7 +8,18 @@
 </head>
 <body>
     <h1>文章首頁</h1>
-    {{ $posts }}
+    @foreach($posts as $post)
+    <div>
+        <h2>{{$post -> title}}</h2>
+        <div>
+            {{$post -> content}}
+        </div>
+        <div>
+            最後更新時間 {{$post -> updated_at}}
+        </div>
+    </div>
+    <hr>
+    @endforeach
     <a href="/create">新增文章</a>
 </body>
 </html>
