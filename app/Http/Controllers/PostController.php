959d18baa53999d937e65ba87e4907e30cc0bcd1
Zac <a19860101@gmail.com>
2021-08-02 20:09:41 +0800

    20210802

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,6 +38,7 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+        return $request;
     }
 
     /**
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -8,5 +8,18 @@
 </head>
 <body>
     <h1>新增文章</h1>
+    <form action="/post" method="post">
+        @csrf
+        <div>
+            <label for="title">文章標題</label>
+            <input type="text" name="title">
+        </div>
+        <div>
+            <label for="content">內文</label>
+            <textarea name="content" id="content" cols="30" rows="10"></textarea>
+        </div>
+        <input type="submit" value="新增">
+
+    </form>
 </body>
 </html>
