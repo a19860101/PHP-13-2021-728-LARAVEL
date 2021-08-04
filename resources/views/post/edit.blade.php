a2637c57078759a4fe1be21ef89a04549ef267a5
Zac <a19860101@gmail.com>
2021-08-04 19:08:08 +0800

    20210804

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -8,6 +8,19 @@
 </head>
 <body>
     <h1>編輯</h1>
-    {{$post}}
+    <form action="" method="post">
+        @csrf
+        <div>
+            <label for="">標題</label>
+            <input type="text" name="title" value="{{$post->title}}">
+        </div>
+        <div>
+            <label for="">內文</label>
+            <textarea name="content" id="" cols="30" rows="10">{{$post->content}}</textarea>
+        </div>
+
+        <input type="submit" value="儲存">
+        <input type="button" value="取消" onclick="history.back()">
+    </form>
 </body>
 </html>
