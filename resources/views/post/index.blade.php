8ec3f761c6aa13e799a63b834c58a861d51c8658
Zac <a19860101@gmail.com>
2021-08-04 20:23:42 +0800

    20210804

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,32 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <nav>
-        <a href="/create">新增文章</a>
-        <a href="{{route('post.create')}}">新增文章</a>
-    </nav>
+@extends('template.master')
 
-    @foreach($posts as $post)
+@section('content')
+
+@foreach($posts as $post)
+<div>
+    <h2>{{$post -> title}}</h2>
     <div>
-        <h2>{{$post -> title}}</h2>
+        {{$post -> content}}
         <div>
-            {{$post -> content}}
-            <div>
-                <a href="/post/{{$post->id}}">繼續閱讀</a>
-                <a href="{{route('post.show',['post' => $post->id])}}">繼續閱讀</a>
-            </div>
-        </div>
-        <div>
-            最後更新時間 {{$post -> updated_at}}
+            <a href="/post/{{$post->id}}">繼續閱讀</a>
+            <a href="{{route('post.show',['post' => $post->id])}}">繼續閱讀</a>
         </div>
     </div>
-    <hr>
-    @endforeach
-</body>
-</html>
+    <div>
+        最後更新時間 {{$post -> updated_at}}
+    </div>
+</div>
+<hr>
+@endforeach
+
+@endsection
